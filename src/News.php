19549d381120efba9309c46c37bc3ef65eebe046
gsemelman <grant3@gmail.com>
2019-02-15 14:06:15 -0500

    updated news

--- src/News.php
+++ src/News.php
@@ -1,27 +1,20 @@
-<div class = "news-image"><img class="rounded mx-auto d-block" src="./assets/img/news/capscup.jpg" alt="News Image"/></div>
+<div class = "news-image"><img class="rounded mx-auto d-block" src="./assets/img/news/cehlfa.jpg" alt="News Image"/></div>
 <div class = "news-text">
 
-	<p class="medium-paragraph">Live from CEHL HQ - Jan 3rd, 2019<p>
-	
-	<p class="medium-paragraph">The Washington Capitals have been crowned season 26 Smitty Cup Champions!!!</p>
-	
+	<p class="medium-paragraph">Live from CEHL HQ - Feburary 15th, 2019<p>
 
-	<p class="small-paragraph text-left">After a long battle of wills, sweat, tears, and possibly dark motives, 
-	 the Washington Capitals have won the smitty cup for the first time in franchise history. </p>
+	<p class="small-paragraph text-left">The offseason activities continue and Free Agency is set to begin this Sunday February 17th at 9PM EST! </p>
 	
-	<p class="small-paragraph text-left">In a weird turn of events, Anaheim GM Dan Rudd was ejected from the Game 7 final for attempting to throw rocks from his box seats
-	on capitals players in retribution for the gooning of Patrick Kane earlier in the series. We attempted to reach out to Caps GM Grant Semelman
-	for comment, but he would not return our phone calls. He was later spotted hiding under his office desk with tears of joy, eating pie 
-	with a mysterious white powder on his face. The Capitals PR team also declined to comment.</p>	
+	<p class="small-paragraph text-left">Please make your way to the two Free Agency pages on site (League -> Free Agents). 
+	The Free Agents section is a list of all the available Free Agents, and a email link to the Agent for that player.</p>	
 	
-	<p class="small-paragraph text-left">Entering the offseason there is still much to do. FA is on the horizon, but first the much anticipated
-	 CEHL draft will take place Sunday January 6th at 1PM. </p>
-	
-	<p class="small-paragraph text-left">You can either navigate to the draft page and find the 'Enter 2017 Draft' button, or you can follow the link below.</p>
+	<p class="small-paragraph text-left">The Free Agent Offers page (League -> Free Agent Offers) is a list of 
+    the current winning offers for each player by Division/Agent.</p>
 
-	<p class="small-paragraph text-left">Good luck to those of you with picks!</p>
+	<p class="small-paragraph text-left">Good luck in Free Agency!</p>
 
-	<a href="https://docs.google.com/spreadsheets/d/1hjOwb0M1nsEomyFvQL6dLpj0rSGLDTZ_vZS1p2QmN3c/edit?usp=sharing" target="_blank" class="btn btn-info" role="button">Enter 2017 Draft</a>
+    <a href="FreeAgents.php" class="btn btn-info" role="button">View Free Agents</a>
+	<a href="FreeAgency.php" class="btn btn-info" role="button">View Free Agent Offers</a>
 			
 
 </div>
