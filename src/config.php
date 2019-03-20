42d627cc57207262d1d02d9c5be4a58624689318
gsemelman <grant3@gmail.com>
2019-03-20 14:40:04 -0400

    coach changes

--- src/config.php
+++ src/config.php
@@ -1,22 +1,18 @@
 ﻿<?php
 
 
-// Langue | Français: 'FR' | English: 'EN'
+// Language | Français: 'FR' | English: 'EN'
 $leagueLang = 'EN';
 
-// Chemin de destination des fichiers html de votre ligue, même dossier '';, répertoire précédant '../';
 // Enter where your HTML files are located in your website. Same folder = '';, Previous directory = '../';
 // Ex: '../html/'; -> Your HTML files folder: http://yourLeague.com/html/ -> Add-on folder: http://yourLeague.com/TablePage/
 $folder = 'transfer/';
 
-// Chemin de destination des fichiers html des parties, à partir de votre répertoire précédant $folder, si tout vos fichiers sont dans le même dossier mettre '';
 // If your games files aren't in the same directory as the other files, enter here the subfolder of $folder.
 // If Everything is in the same folder enter '';
 $folderGames = '';
 
 # SALARY COP
-// 0 : Calcul de la masse salariale avec le Pro payroll seulement
-// 1 : Calcul de la masse salariale avec le Pro + Farm Payroll
 // 0 : Calculate Salary Cap with only Pro Payroll
 // 1 : Calculate Salary Cap with Pro + Farm Payroll
 $leagueSalaryIncFarm = 0;
@@ -27,19 +23,14 @@ $leagueSalaryCap = 53000000;
 // Alerte Proche du Plafond Salariale | Warning Near Salary Cap | Ex: 55000000 for 55M$
 $leagueSalaryClose = 52000000;
 
-// Plafond salariale minimum | Floor Salary Cap | Ex: 45000000 for 45M$ | 0 : Désactivé/Unactivated
+// Floor Salary Cap | Ex: 45000000 for 45M$ | 0 : Inactivated
 $leagueSalaryCapFloor = 0;
 
-// Point en Prolongation | Overtime Point | 0:Off | 1:On (One Point)
+// Overtime Point | 0:Off | 1:On (One Point)
 $leagueOvertimePoint = 1;
 
 # CAREER STATS
-// Needed Files in each season folders : GMs, PlayerVitals, TeamScoring, PLFTeamScoring, Standings
-
-// Chemin d'accès aux dossiers contenants les anciennes saisons. Mettre / à la fin. 0 = désactivé
-// Ce dossier doit avoir le même nom et avoir le chiffre de la saison, example: Season 1, Season 2, Season 3. Mettre # à la place du chiffre!
-// Example complète: ../backup/Season #/
-// De plus, le dossier de la saison actuelle ne doit pas être ici. Sinon, les stats vont être chargés deux fois.
+// Needed Files in each season folders : GMs, PlayerVitals, TeamScoring, PLFTeamScoring, Standings, Schedule
 
 // Enter where your backup HTML files are located in your website. Unabled : 0;
 // This folder MUST have the same name and at the end, the number of the season. Ex: Season 1, Season 2, Season 3. Put # to replace the number!
@@ -49,7 +40,6 @@ $leagueOvertimePoint = 1;
 $folderCarrerStats = 'backup/season#/';
 
 # BOXSCORE LOGO
-// Logo optimal specs: width:60px, height:30px (The image will be resized if to big in either direction)
 // Supported Formats: JPEG, GIF, PNG, BMP, ICO (PNG Recommanded)
 // Logo names: the same as the name in your FHLsim but ALL the characters in lowercase Ex: RED WINGS = red wings
 // Enter where your team logos are located in your website. Same folder = '';, Previous directory = '../';
@@ -76,7 +66,7 @@ $leagueFuturesDraftYears = 4;
 $playoffMode = 1;
 
 
-// Bleu LHSX - LHSX Blue : 1
+// Blue : 1
 $selectedColor = 1;
 // Ne pas modifier - Don't modify
 include 'config_color'.$selectedColor.'.php';
