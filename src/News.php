7305dd638ddad9d7313e0231967ccb5c65470c62
gsemelman <grant3@gmail.com>
2019-04-18 17:32:19 -0400

    Update News.php

--- src/News.php
+++ src/News.php
@@ -6,7 +6,7 @@
 	<p class="small-paragraph text-left">The season has started, and the Oilers and Rangers are both off to hot starts while leading their respective conferences</p>
 	
 	<p class="small-paragraph text-left">There have also been some surprising starts, perennial contenders Anaheim and Chicago are off to shaky starts, 
-	while the so called tankers the Calgary flames are having trouble losing games. St Louis with their rag team youth driven team has also seen a great start to the season</p>
+	while the so called tankers the Calgary flames are having trouble losing games. St Louis with their rag tag youth driven team has also seen a great start to the season</p>
 		
 	<p class="small-paragraph text-left">The site continues to see changes for the better, with a brand new boxscore page, along with general theming of the website. Please send
 	your complaints to CEHL PR representative Josh Gordon should you not like anything.</p>
