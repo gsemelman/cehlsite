edafe5b6e68c72b42364f53eb78ada23c0a447b4
gsemelman <grant3@gmail.com>
2019-04-18 17:22:36 -0400

    updatse

--- src/News.php
+++ src/News.php
@@ -1,16 +1,15 @@
-<div class = "news-image"><img class="rounded mx-auto d-block" src="./assets/img/news/waivers.jpg" alt="News Image"/></div>
+<div class = "news-image"><img class="rounded mx-auto d-block" src="./assets/img/news/rangers_oilers.jpg" alt="News Image"/></div>
 <div class = "news-text">
 
-	<p class="medium-paragraph">Live from CEHL HQ - March 27th, 2019<p>
+	<p class="medium-paragraph">Live from CEHL HQ - April 18th, 2019<p>
 
-	<p class="small-paragraph text-left">The CEHL Waiver draft will be held March 27th</p>
+	<p class="small-paragraph text-left">The season has started, and the Oilers and Rangers are both off to hot starts while leading their respective conferences</p>
 	
-	<p class="small-paragraph text-left">Please follow the link to view the available players</p>	
-	
-
-    <a href="WaiverDraft.php" class="btn btn-outline-primary" role="button">View Waiver Draft</a>
-			
-
+	<p class="small-paragraph text-left">There have also been some surprising starts, perennial contenders Anaheim and Chicago are off to shaky starts, 
+	while the so called tankers the Calgary flames are having trouble losing games. St Louis with their rag team youth driven team has also seen a great start to the season</p>
+		
+	<p class="small-paragraph text-left">The site continues to see changes for the better, with a brand new boxscore page, along with general theming of the website. Please send
+	your complaints to CEHL PR representative Josh Gordon should you not like anything.</p>
 </div>
 
 <style>
